bd8a97b11057eac36e7e16151cc4ea107a7fef31
Arkady G <gude@list.ru>
2020-12-24 10:45:42 +0300

    Сайт «‎Дом питомца» конструктор класса Cat вариант 02

--- cat_list.py
+++ cat_list.py
@@ -1,14 +1,12 @@
 from cat import Cats
 
 cat_1 = Cats('Барон', 'мальчик', '2 года')
-
-print('Имя -', cat_1.name)
-print('Пол -', cat_1.gender)
-print('Возраст -', cat_1.age)
-print()
-
 cat_2 = Cats('Сэм', 'мальчик', '2 года')
 
-print('Имя -', cat_2.name)
-print('Пол -', cat_2.gender)
-print('Возраст -', cat_2.age)
+pets = [cat_1, cat_2]
+
+for pet in pets:
+    print('Имя -', pet.getname())
+    print('Пол -', pet.getgender())
+    print('Возраст -', pet.getage())
+    print()
